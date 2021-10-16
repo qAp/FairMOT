eeaf8cf4e06d341ce813a15814e4b5cf5daf8a5b
Jack Yu <jackchungchiehyu@gmail.com>
2021-10-16 18:20:03 +0800

    Check start_epoch after loading pre-trained model

--- src/train.py
+++ src/train.py
@@ -63,6 +63,7 @@ def main(opt):
     if opt.load_model != '':
         model, optimizer, start_epoch = load_model(
             model, opt.load_model, trainer.optimizer, opt.resume, opt.lr, opt.lr_step)
+        print('ZZZZZZ start_epoch = ', start_epoch)
 
     for epoch in range(start_epoch + 1, opt.num_epochs + 1):
         mark = epoch if opt.save_all else 'last'
